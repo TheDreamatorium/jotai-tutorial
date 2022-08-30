a2ff941c81fb22cdeefd43d6d09397db218089b9
david <you@example.com>
2022-08-30 10:35:33 +0400

    bootstrap + css

--- src/Count.js
+++ src/Count.js
@@ -9,10 +9,14 @@ const Count = () => {
     const [isCountEven] = useAtom(isCountEvenAtom);
 
     return (
-        <div>
-            Actual:{count} - Double: {doubleCount} - is even: {isCountEven ? 'true' : 'false'}
-            <button onClick={increment}>Add</button>
-            <button onClick={decrement}>Substract</button>
+        <div className="d-flex flex-column w-25 ms-auto me-auto">
+            <div className='list-group w-100'>
+                 <div className='list-group-item'>Actual: {count}</div>
+                 <div className='list-group-item'>Double: {doubleCount}</div>
+                 <div className='list-group-item'>is even: {isCountEven ? 'true' : 'false'}</div>
+            </div>
+            <button className='btn btn-success w-100' onClick={increment}>Add</button>
+            <button className='btn btn-danger w-100' onClick={decrement}>Substract</button>
         </div>
     )
 }
